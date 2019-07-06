e8f8f8d03309dbca14780a1e34bfd3d3d706ee9d
Yuval Ron <yuvaly2394@gmail.com>
2019-07-06 14:36:08 +0300

    Create separate component for new event form

---
+++ src/events/components/NewEventForm.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import TextField from '@material-ui/core/TextField'
+import FormContainer from '../../commons/components/FormContainer'
+
+class NewEventForm extends Component {
+  render() {
+    const {newEvent, createOnChangeCallback, formButtons, room} = this.props
+
+    return (
+      <FormContainer title={`Create new event in room: ${room.name}`} buttons={formButtons}>
+        <TextField
+          id="name"
+          label="Event name"
+          value={newEvent.name}
+          style={{width: '230px'}}
+          onChange={createOnChangeCallback('name')}
+        />
+        <TextField
+          id="startTime"
+          label="Event start time"
+          type="datetime-local"
+          value={newEvent.startTime}
+          InputLabelProps={{shrink: true}}
+          style={{width: '230px'}}
+          onChange={createOnChangeCallback('startTime')}
+        />
+        <TextField
+          id="endTime"
+          label="Event end time"
+          type="datetime-local"
+          value={newEvent.endTime}
+          InputLabelProps={{shrink: true}}
+          style={{width: '230px'}}
+          onChange={createOnChangeCallback('endTime')}
+        />
+        <TextField
+          id="ownerId"
+          label="Event owner id"
+          value={newEvent.ownerId}
+          style={{width: '230px'}}
+          onChange={createOnChangeCallback('ownerId')}
+        />
+      </FormContainer>
+    )
+  }
+}
+
+export default NewEventForm
--- src/rooms/components/RoomPage.js
+++ src/rooms/components/RoomPage.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import moment from 'moment'
-import TextField from '@material-ui/core/TextField'
-import FormContainer from '../../commons/components/FormContainer'
+import NewEventForm from '../../events/components/NewEventForm'
 
 class RoomPage extends Component {
   state = {
@@ -41,42 +40,16 @@ class RoomPage extends Component {
       return null
     }
 
-    return (
-      <FormContainer title={`Create new event in room: ${room.name}`} buttons={formButtons}>
-        <TextField
-          id="name"
-          label="Event name"
-          value={newEvent.name}
-          onChange={this.createOnChangeCallback('name')}
-        />
-        <TextField
-          id="startTime"
-          label="Event start time"
-          type="datetime-local"
-          value={newEvent.startTime}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={this.createOnChangeCallback('startTime')}
-        />
-        <TextField
-          id="endTime"
-          label="Event end time"
-          type="datetime-local"
-          value={newEvent.endTime}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={this.createOnChangeCallback('endTime')}
-        />
-        <TextField
-          id="ownerId"
-          label="Event owner id"
-          value={newEvent.ownerId}
-          onChange={this.createOnChangeCallback('ownerId')}
-        />
-      </FormContainer>
-    )
+    return null
+
+    // return (
+    //   <NewEventForm
+    //     newEvent={newEvent}
+    //     room={room}
+    //     formButtons={formButtons}
+    //     createOnChangeCallback={this.createOnChangeCallback}
+    //   />
+    // )
   }
 }
 
