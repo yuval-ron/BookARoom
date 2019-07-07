65c90bf8e0c97aa56ffa4f261ba033e6b6b0935c
Yuval Ron <yuvaly2394@gmail.com>
2019-07-07 03:58:13 +0300

    Updates

---
+++ precache-manifest.10c01ed8964f5764734226e9c9148877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50655d4dcf9d0c02c66055f549c9dd2e",
+    "url": "/Bookaroom/index.html"
+  },
+  {
+    "revision": "a6abf32258627b6eb7df",
+    "url": "/Bookaroom/static/css/main.ce22374a.chunk.css"
+  },
+  {
+    "revision": "5ba7e1319be505abe034",
+    "url": "/Bookaroom/static/js/2.c6d42939.chunk.js"
+  },
+  {
+    "revision": "a6abf32258627b6eb7df",
+    "url": "/Bookaroom/static/js/main.8a074a0f.chunk.js"
+  },
+  {
+    "revision": "26a01c6d5b2b13cde54e",
+    "url": "/Bookaroom/static/js/runtime~main.c8f4c6d7.js"
+  }
+]);
